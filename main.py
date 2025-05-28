a761f910dbb27f2b2e10c526d36210815164caf5
AruukeNurpaiaskyzy <aruukelisa@gmail.com>
2025-05-28 22:01:35 +0600

    tried my code without solid

---
+++ main.py
@@ -0,0 +1,18 @@
+# # SOLID
+# class Report:
+#     def generate(self):
+#         return "Отчет"
+    
+# class ReportSaver:
+#     def save_to_file(self, content):
+#         with open ("report.txt" 'w') as f:
+#                    f.write(content)
+
+
+class Orders:
+    def add(self):
+        return "добавлено"
+    def show(self):
+        return'показано"
+    def save(self):
+        return "сохранено"
