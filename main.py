76134d521c2879eb54cbd4cc753dea6442ccf521
AruukeNurpaiaskyzy <aruukelisa@gmail.com>
2025-05-28 22:25:12 +0600

    solid-s

--- main.py
+++ main.py
@@ -9,10 +9,48 @@
 #                    f.write(content)
 
 
-class Orders:
-    def add(self):
-        return "добавлено"
-    def show(self):
-        return'показано"
-    def save(self):
-        return "сохранено"
+# class Orders:
+#     def add(self):
+#         return "добавлено"
+#     def show(self):
+#         return'показано"
+#     def save(self):
+#         return "сохранено"
+
+
+class Order:
+    def __init__(self):
+        self.items = []
+
+    def add_item(self, item):
+        self.items.append(item)
+
+    def get_items(self):
+        return self.items
+
+
+class OrderPrinter:
+    def print(self, order: Order):
+        for item in order.get_items():
+            print(f"Блюдо: {item}")
+
+
+class OrderSaver:
+    def save_to_file(self, order: Order, filename="order.txt"):
+        with open(filename, "w") as f:
+            for item in order.get_items():
+                f.write(f"{item}\n")
+
+
+
+order = Order()
+order.add_item("Пицца")
+order.add_item("Салат")
+
+
+
+printer = OrderPrinter()
+printer.print(order)
+
+saver = OrderSaver()
+saver.save_to_file(order)
