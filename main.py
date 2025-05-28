dd9f1b46d9042f8a95ba6bfdf3ff20f3ef24c9c0
AruukeNurpaiaskyzy <aruukelisa@gmail.com>
2025-05-28 22:53:52 +0600

    solid-o

--- main.py
+++ main.py
@@ -18,6 +18,47 @@
 #         return "сохранено"
 
 
+# class Order:
+#     def __init__(self):
+#         self.items = []
+
+#     def add_item(self, item):
+#         self.items.append(item)
+
+#     def get_items(self):
+#         return self.items
+
+
+# class OrderPrinter:
+#     def print(self, order: Order):
+#         for item in order.get_items():
+#             print(f"Блюдо: {item}")
+
+
+# class OrderSaver:
+#     def save_to_file(self, order: Order, filename="order.txt"):
+#         with open(filename, "w") as f:
+#             for item in order.get_items():
+#                 f.write(f"{item}\n")
+
+
+
+# order = Order()
+# order.add_item("Пицца")
+# order.add_item("Салат")
+
+
+
+# printer = OrderPrinter()
+# printer.print(order)
+
+# saver = OrderSaver()
+# saver.save_to_file(order)
+
+
+from abc import ABC, abstractmethod
+import json
+
 class Order:
     def __init__(self):
         self.items = []
@@ -29,28 +70,30 @@ class Order:
         return self.items
 
 
-class OrderPrinter:
-    def print(self, order: Order):
-        for item in order.get_items():
-            print(f"Блюдо: {item}")
+class OrderSaver(ABC):
+    @abstractmethod
+    def save(self, order: Order):
+        pass
 
 
-class OrderSaver:
-    def save_to_file(self, order: Order, filename="order.txt"):
-        with open(filename, "w") as f:
+class TextOrderSaver(OrderSaver):
+    def save(self, order: Order):
+        with open("order.txt", "w") as f:
             for item in order.get_items():
                 f.write(f"{item}\n")
 
 
+class JsonOrderSaver(OrderSaver):
+    def save(self, order: Order):
+        with open("order.json", "w") as f:
+            json.dump(order.get_items(), f)
 
 order = Order()
-order.add_item("Пицца")
-order.add_item("Салат")
+order.add_item("Суп")
+order.add_item("Чай")
 
+saver = JsonOrderSaver()
+saver.save(order)
 
-
-printer = OrderPrinter()
-printer.print(order)
-
-saver = OrderSaver()
-saver.save_to_file(order)
+saver2 = TextOrderSaver()
+saver2.save(order)
